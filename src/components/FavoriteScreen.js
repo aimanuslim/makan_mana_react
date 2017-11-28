04f8daffadc2084e5a791fb16714ad16ab8af2ca
Aiman Md Uslim <amduslim@pudue.edu>
2017-11-28 23:11:31 +0800

    Added favorite screen

---
+++ src/components/FavoriteScreen.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, ScrollView } from 'react-native';
+import { connect } from 'react-redux';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+import { Button, CardSection } from './common';
+import AreaList from './AreaList';
+
+class FavoriteScreen extends Component {
+	render() {
+		return (
+			<View style={{ flex:1 }}>
+			<KeyboardAwareScrollView
+						keyboardShouldPersistTaps='handled'
+			>
+						<ScrollView>
+							<AreaList style={{ flex: 1 }} data={{}} />
+						</ScrollView>
+			</KeyboardAwareScrollView>
+			<CardSection>
+				<Button>Choose for me</Button>		
+			</CardSection>
+			</View>
+			);
+	}
+}
+
+
+export default FavoriteScreen;
